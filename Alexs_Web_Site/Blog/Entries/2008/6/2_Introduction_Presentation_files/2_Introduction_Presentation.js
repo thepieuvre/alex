f5935aa40e3f016f60eb6d6ea07a24deca01e3d4
Alexandre di Costanzo <alex@thepieuvre.com>
2013-07-18 22:55:21 +0200

    iWeb: Melbourne uni version

---
+++ Alexs_Web_Site/Blog/Entries/2008/6/2_Introduction_Presentation_files/2_Introduction_Presentation.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20130718
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,215),url:'2_Introduction_Presentation_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'2_Introduction_Presentation_files/stroke_1.png'},{rect:new IWRect(5,-5,290,10),url:'2_Introduction_Presentation_files/stroke_2.png'},{rect:new IWRect(295,-5,10,10),url:'2_Introduction_Presentation_files/stroke_3.png'},{rect:new IWRect(295,5,10,215),url:'2_Introduction_Presentation_files/stroke_4.png'},{rect:new IWRect(295,220,10,10),url:'2_Introduction_Presentation_files/stroke_5.png'},{rect:new IWRect(5,220,290,10),url:'2_Introduction_Presentation_files/stroke_6.png'},{rect:new IWRect(-5,220,10,10),url:'2_Introduction_Presentation_files/stroke_7.png'}],new IWSize(300,225))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('2_Introduction_Presentation_files/2_Introduction_PresentationMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
