313aa5f195e0d1b06a3e6d6a9b3aa8444d528e7e
Alexandre di Costanzo <alex@thepieuvre.com>
2013-07-18 22:40:39 +0200

    Initial version

---
+++ Alexs_Web_Site/My_Research_files/My_Research.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130718
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('My_Research_files/My_ResearchMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
